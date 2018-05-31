a97fbbf647b909485376bb26faf965f22d5d63b8
Ruchi Patel <ruchipatel91@gmail.com>
2018-05-31 01:34:28 -0500

    Populate makeCacheMatrix and cacheSolve functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,51 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+#Ruchi Patel 5/30/2018
+#Programming assignment 2(PEER GRADED)
+#Inverse the matrix by caching
 
 makeCacheMatrix <- function(x = matrix()) {
 
+  i <- NULL
+  
+  set <- function(y) { #SET THE FUNCTION
+    x <<- y
+    i <<- NULL #INITIATES THE VECTOR
+  }
+  
+  get <- function() x #GET THE FUNCTION
+  
+  set_inverse <- function(inverse) i<<- inverse
+  
+  get_inverse <- function() i
+  
+  list(set = set, get = get,
+       set_inverse = set_inverse,
+       get_inverse = get_inverse)
+  
+  
+  
 }
 
-
-## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    
+  
+  i <- x$get_inverse() #GET THE PREVIOUS VALUE OF CACHED MATRIX
+  
+  if(!is.null(i)) { # checks the condition for i... if it has chached value then it will print msg.
+    message("Getting cached MATRIX")
+    return(i)
+  }
+ 
+  m<- x$get()#GET THE MATRIX PASSED TO THE FUNCTION
+  
+  i <- solve(m, ...) #GIVES INVERTED MATERIX
+  
+  x$set_inverse(i) #SET VALUE OF INVERTED MATRIX
+  
+  i #RETURN INVERTED MATRIX
+  
+  
 }
+
+
+
